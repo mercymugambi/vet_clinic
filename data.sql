b69760ec3dc63a7ee7f2baf6c17d7adc46899f20
mercymugambi <mercymugambi2016@gmail.com>
2023-06-08 11:21:41 +0300

    Insert Plantmon data

--- data.sql
+++ data.sql
@@ -6,4 +6,5 @@ VALUES
   ('Gabumon', '2018-11-15', 2, true, 8),
   ('Pikachu', '2021-01-07', 1, false, 15.04),
   ('Devimon', '2017-05-12', 5, true, 11),
-  ('Charmander', '2020-02-08', 0, false, -11, '');
+  ('Charmander', '2020-02-08', 0, false, -11, ''),
+  ('Plantmon', '2021-11-15', 2, true, -5.7, '');
