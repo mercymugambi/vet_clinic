67ee5daf3e1c102bd83bfa9dfeb2042ddd8650db
mercymugambi <mercymugambi2016@gmail.com>
2023-06-08 11:19:54 +0300

    Update data with values

--- data.sql
+++ data.sql
@@ -5,4 +5,5 @@ VALUES
   ('Agumon', '2020-02-03', 0, true, 10.23),
   ('Gabumon', '2018-11-15', 2, true, 8),
   ('Pikachu', '2021-01-07', 1, false, 15.04),
-  ('Devimon', '2017-05-12', 5, true, 11);
+  ('Devimon', '2017-05-12', 5, true, 11),
+  ('Charmander', '2020-02-08', 0, false, -11, '');
