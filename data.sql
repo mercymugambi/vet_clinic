a7aab324472547ba66af18a53afea6bc4b3010ed
mercymugambi <mercymugambi2016@gmail.com>
2023-06-09 15:57:52 +0300

    Insert data to specialization table

--- data.sql
+++ data.sql
@@ -70,3 +70,31 @@ VALUES ('Stephanie Mendez', 64, '1981-05-04');
 
 INSERT INTO vets (name, age, date_of_graduation)
 VALUES ('Jack Harkness', 38, '2008-06-08');
+
+-- Vet William Tatcher specializes in Pokemon
+INSERT INTO specializations (vet_id, species_id)
+VALUES (
+  (SELECT id FROM vets WHERE name = 'William Tatcher'),
+  (SELECT id FROM species WHERE name = 'Pokemon')
+);
+
+-- Vet Stephanie Mendez specializes in Digimon and Pokemon
+INSERT INTO specializations (vet_id, species_id)
+VALUES (
+  (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+  (SELECT id FROM species WHERE name = 'Digimon')
+);
+
+INSERT INTO specializations (vet_id, species_id)
+VALUES (
+  (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+  (SELECT id FROM species WHERE name = 'Pokemon')
+);
+
+-- Vet Jack Harkness specializes in Digimon
+INSERT INTO specializations (vet_id, species_id)
+VALUES (
+  (SELECT id FROM vets WHERE name = 'Jack Harkness'),
+  (SELECT id FROM species WHERE name = 'Digimon')
+);
+
